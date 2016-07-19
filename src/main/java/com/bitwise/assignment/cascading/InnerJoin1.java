5756a6a5fc3ed961f5318e749e8eeacb7888692d
exorcist007 <Aniruddha.Sinha@bitwiseglobal.com>
2016-07-19 18:07:37 +0530

    Initial Commit

---
+++ src/main/java/com/bitwise/assignment/cascading/InnerJoin1.java
@@ -0,0 +1,18 @@
+package com.bitwise.assignment.cascading;
+
+import cascading.pipe.CoGroup;
+import cascading.pipe.Pipe;
+import cascading.pipe.SubAssembly;
+import cascading.pipe.joiner.InnerJoin;
+import cascading.tuple.Fields;
+
+public class InnerJoin1 extends SubAssembly {
+	 public InnerJoin1(Pipe sourcePipe1, Pipe sourcePipe2) {
+         setPrevious(sourcePipe1,sourcePipe2);
+
+         Pipe join = new CoGroup(sourcePipe1,
+                 new Fields("acc_no","name","dob","phone_no"),sourcePipe2,
+                 new Fields("acc_no1","trans_type","trans_amt","trans_date"),new InnerJoin());
+         setTails(join);
+     }
+}
