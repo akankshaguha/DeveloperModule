db8720e7e4987dee99c616f8490defa3f198e718
Aniruddha Sinha <Aniruddha.Sinha@bitwiseglobal.com>
2016-08-04 16:10:55 +0530

    Update InnerJoin1.java

--- src/main/java/com/bitwise/assignment/cascading/InnerJoin1.java
+++ src/main/java/com/bitwise/assignment/cascading/InnerJoin1.java
@@ -8,12 +8,12 @@ import cascading.tuple.Fields;
 
 public class InnerJoin1 extends SubAssembly {
 	 public InnerJoin1(Pipe sourcePipe1, Pipe sourcePipe2) {
-         setPrevious(sourcePipe1,sourcePipe2);
+          setPrevious(sourcePipe1,sourcePipe2);
 
-         Pipe join = new CoGroup(sourcePipe1,
-                 new Fields("acc_no","name","dob","phone_no"),sourcePipe2,
-                 new Fields("acc_no1","trans_type","trans_amt","trans_date"),new InnerJoin());
-         setTails(join);
+            Pipe join = new CoGroup(sourcePipe1,
+                    new Fields("acc_no","name","dob","phone_no"),sourcePipe2,
+                    new Fields("acc_no1","trans_type","trans_amt","trans_date"),new InnerJoin());
+            setTails(join);
      }
 }
 //only this subassembly to be tested within this file
