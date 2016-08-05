65aef36f659d9e0824725ff028996eef3d963d73
exorcist007 <Aniruddha.Sinha@bitwiseglobal.com>
2016-08-05 12:43:10 +0530

    Added new class and tests

--- src/main/java/com/bitwise/assignment/cascading/InnerJoin1.java
+++ src/main/java/com/bitwise/assignment/cascading/InnerJoin1.java
@@ -1,19 +1,16 @@
 package com.bitwise.assignment.cascading;
 
-import cascading.pipe.CoGroup;
-import cascading.pipe.Pipe;
-import cascading.pipe.SubAssembly;
-import cascading.pipe.joiner.InnerJoin;
-import cascading.tuple.Fields;
-
-public class InnerJoin1 extends SubAssembly {
-	 public InnerJoin1(Pipe sourcePipe1, Pipe sourcePipe2) {
-          setPrevious(sourcePipe1,sourcePipe2);
-
-            Pipe join = new CoGroup(sourcePipe1,
-                    new Fields("acc_no","name","dob","phone_no"),sourcePipe2,
-                    new Fields("acc_no1","trans_type","trans_amt","trans_date"),new InnerJoin());
-            setTails(join);
-     }
+public class InnerJoin1{
+    private String s;
+    private int a;
+    public InnerJoin1(){
+        s="Aniruddha";
+        a=23;
+    }
+    public InnerJoin1(String name,int age){
+        s=name;
+        a=age;
+    }
+    public String getName(){return s;}
+    public int getAge(){return a;}
 }
-//only this subassembly to be tested within this file
