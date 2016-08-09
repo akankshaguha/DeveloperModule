ff0038178cce3d0c1081d3338b957f19ea1960ce
akankshag <Akanksha.Guha@bitwiseglobal.com>
2016-08-09 13:05:40 +0530

    Code added

--- src/main/java/com/bitwise/assignment/cascading/InnerJoin1.java
+++ src/main/java/com/bitwise/assignment/cascading/InnerJoin1.java
@@ -1,16 +1,26 @@
 package com.bitwise.assignment.cascading;
 
-public class InnerJoin1{
-    private String s;
-    private int a;
-    public InnerJoin1(){
-        s="Aniruddha";
-        a=23;
+import cascading.operation.Identity;
+import cascading.pipe.CoGroup;
+import cascading.pipe.Each;
+import cascading.pipe.Pipe;
+import cascading.pipe.SubAssembly;
+import cascading.tuple.Fields;
+
+public class InnerJoin1 extends SubAssembly{
+
+    public InnerJoin1(Pipe sourcePipe1, Pipe sourcePipe2) {
+        setPrevious(sourcePipe1);
+        setPrevious(sourcePipe2);
+
+        Pipe pipe1 = new Each(sourcePipe1,new Fields("acc_no","name","dob","phone_no"),new Identity());
+
+        Pipe  pipe2 = new Each(sourcePipe2,new Fields("acc_no1","trans_type","trans_amt","trans_date"),new Identity());
+
+        Pipe join = new CoGroup(pipe1,pipe2);
+
+        // Pipe join1 = new Each(join,new Identity());
+        setTails(join);
+
     }
-    public InnerJoin1(String name,int age){
-        s=name;
-        a=age;
-    }
-    public String getName(){return s;}
-    public int getAge(){return a;}
 }
--- src/test/java/com/bitwise/assignment/cascading/InnerJoin1Test.java
+++ src/test/java/com/bitwise/assignment/cascading/InnerJoin1Test.java
@@ -1,17 +1,61 @@
 package com.bitwise.assignment.cascading;
 
+import cascading.pipe.Pipe;
+import cascading.tuple.Fields;
+import cascading.tuple.Tuple;
+import com.hotels.plunger.Bucket;
+import com.hotels.plunger.Data;
+import com.hotels.plunger.DataBuilder;
+import com.hotels.plunger.Plunger;
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.List;
 
 public class InnerJoin1Test{
+
     @Test
-    public void itShouldReturnTheCorrectDefaultNameAndAge(){
-        //given
-        InnerJoin1 x=new InnerJoin1();
-        //when
-        assertEquals("Aniruddha",x.getName());
-        assertEquals(23,x.getAge());
+    public void itShouldPerformTheJoinOfTwoPipes() {
+        Plunger plungerTestApi=new Plunger();
+        Plunger plunger = new Plunger();
+        //Master_Csv
+        Data corpus1 = new DataBuilder(new Fields("acc_no","name","dob","phone_no"
+
+        ))
+                .addTuple(1002002051,"abc1","10/1/1991",888501)
+
+                .build();
+
+        //Transaction.txt
+        Data corpus2 = new DataBuilder(new Fields("acc_no1","trans_type","trans_amt","trans_date"
+        ))
+                .addTuple(1002002051,"debit",500,"5/1/2014")
+
+                .build();
+        //input pipe
+
+        Pipe inputpipe1 = plunger.newNamedPipe("in1", corpus1);
+        Pipe inputpipe2 = plunger.newNamedPipe("in2", corpus2);
+
+
+        Pipe assemblyToTest = new InnerJoin1(inputpipe1,inputpipe2);
+
+        //plunger bucket
+        Bucket bucket = plunger.newBucket(new Fields("acc_no","name","dob","phone_no",
+                "acc_no1","trans_type","trans_amt","trans_date"), assemblyToTest);
+
+
+
+
+        List<Tuple> actual = bucket.result().asTupleList();
+
+        Assert.assertThat( actual.get(0) , CoreMatchers.is(
+                new Tuple(1002002051,"abc1","10/1/1991",888501,
+                        1002002051,"debit",500,"5/1/2014"
+
+                )));
+
+
     }
 }
